1126b7f84a044e0f3fa6c9168fb569d0d070ef61
gashaw mucheye <gashaw2533@gmail.com>
2023-12-17 14:18:37 +0200

    added style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
 const App: React.FC = () => {
-  return <div className="bg-slate-500 text-red-500">App</div>;
+  return (
+    <div className="bg-slate-500 text-red-500 h-screen justify-between flex flex-col text-center">
+      <header className="bg-zinc-700 text-white h-9 py-4">header</header>
+      <main className="flex-1 bg-red-300">main</main>
+      <footer className="bg-zinc-700 text-white text-center">
+        &copy; All Rights Reserved
+      </footer>
+    </div>
+  );
 };
 
 export default App;
