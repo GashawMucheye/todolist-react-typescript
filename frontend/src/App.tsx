2f4accafecfc45342d12183476578e5eb318a134
gashaw mucheye <gashaw2533@gmail.com>
2023-12-19 14:19:55 +0200

    created branch for adding useReducer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,39 @@ import { Alltasks } from './types/types';
 import SingleTodo from './components/SingleTodo';
 import InputFields from './components/InputFields';
 
+// interface State {
+//   filter(arg0: (todo: any) => boolean): unknown;
+
+//   id: number;
+//   todo: string;
+//   isDone: boolean;
+// }
+
+// interface Action {
+//   type: string;
+//   payload?: any;
+// }
+
+// export const reducer = (state: State, action: Action) => {
+//   switch (action.type) {
+//     case 'ADD_TODO':
+//       return [
+//         ...state,
+//         { id: Date.now(), todo: action.payload, isDone: false },
+//       ];
+//     case 'EDIT':
+//       return state.map((todo) =>
+//         todo.id === action.payload
+//           ? { ...todo, completed: !todo.completed }
+//           : todo
+//       );
+//     case 'DONE':
+//       return state.filter((todo) => todo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
   const [allTasks, setAllTasks] = useState<Alltasks[]>([]);
